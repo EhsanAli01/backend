b1a2e04dff00286febe420546e85db4f27a6172f
Ehsan Ali <ehsanali3669@gmail.com>
2024-04-30 08:59:02 +0500

    updated

--- app.js
+++ app.js
@@ -7,23 +7,28 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const cloudinary = require('cloudinary').v2;
 const fileUpload = require('express-fileupload');
-          
-cloudinary.config({ 
-  cloud_name: 'djkufp9s1', 
-  api_key: '174572555582571', 
-  api_secret: 'QI74kpK0jpyEJ-Uh39vsJAxW6x8' 
+
+cloudinary.config({
+    cloud_name: 'djkufp9s1',
+    api_key: '174572555582571',
+    api_secret: 'QI74kpK0jpyEJ-Uh39vsJAxW6x8'
 });
 
-app.use(cors());
+const corsOptions = {
+    origin: 'https://web-wing.vercel.app/',
+    optionsSuccessStatus: 200,
+};
 
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(cors(corsOptions));
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 mongoose.connect('mongodb+srv://ehsanali3669:12345@sbs.c9rzace.mongodb.net/');
-mongoose.connection.on('connected' , connection => {
+mongoose.connection.on('connected', connection => {
     console.log('connection success');
 })
-mongoose.connection.on('error' , err => {
+mongoose.connection.on('error', err => {
     console.log(err);
 })
 
@@ -31,12 +36,12 @@ app.use(fileUpload({
     useTempFiles: true
 }));
 
-app.use('/user' , user);
-app.use('/posts' , posts);
+app.use('/user', user);
+app.use('/posts', posts);
 
 
 // bad request
-app.use((req,res,next) => {
+app.use((req, res, next) => {
     res.status(404).json({
         message: "page not found"
     })
